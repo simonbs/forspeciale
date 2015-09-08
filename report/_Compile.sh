c8eee0823524f0d1f73bb9ab20d2af1954e1e8c4
Thalley <mrthalley@gmail.com>
2015-09-08 21:16:39 +0200

    Added another report template

---
+++ report/_Compile.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Copyright Thalley.com
+echo Choose what you want to do
+echo 1 - Compile fast
+echo 2 - Compile with Table of contents
+echo 3 - Compile with references
+echo D - Delete files
+echo Q - Exit
+read -p "Your choice: [1, 2, 3, D, Q] " choice
+case $choice in
+  3 )
+	pdflatex master.tex
+	bibtex master.tex;&
+  2 )
+	pdflatex master.tex;&
+  1 ) 
+	pdflatex master.tex;&
+  D )
+	rm *.log;
+	rm *.aux;
+	rm *.out;
+	rm *.toc;
+	rm *.gz;
+	rm *.bbl;
+	rm *.blg;
+	rm *.tdo;
+	rm *.brf;
+	rm *.lot;
+	rm *.dvi;
+	rm *.lof;;
+  * ) read -p "Not valid option";;
+esac
